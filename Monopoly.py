26e82437e2c691eab8f1c69dc9a57bea01ec9efa
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2021-01-10 20:15:08 +0100

    Translated everything into english.

--- Monopoly.py
+++ Monopoly.py
@@ -8,14 +8,14 @@ try:
 
 except ValueError:
 
-    print("Enter an integer as a commandline argument")
+    print("Enter an integer as a commandline argument.")
     exit(1)
 
 try:
     max_rounds = int(max_rounds)
 
 except ValueError:
-    print("Enter an integer as a commandline argument")
+    print("Enter an integer as a commandline argument.")
     exit(1)
 
 ###################################################################
@@ -32,7 +32,7 @@ def overflow(max, position, roll):
 
 #############################
 
-def felder():
+def board():
 
     positions = []
 
@@ -48,7 +48,7 @@ def jail(position, positions, log):
     if position == 30:
 
         positions[position] += 1
-        log.write(f"Go to Jail ")
+        log.write(f"Go to Jail. ")
         position = 10
         positions[position] += 1
         return position, positions
@@ -66,14 +66,14 @@ def community_chest(position, positions, log):
     if draw == 1:
 
         positions[position] +=1
-        log.write(f"Community card. Go to Jail ")
+        log.write(f"Community card. Go to Jail. ")
         position = 10
         return position, positions
 
     elif draw == 2:
 
         positions[position] +=1
-        log.write(f"Community card. Go to Go ")
+        log.write(f"Community card. Go to Go. ")
         position = 0
         return position, positions
 
@@ -90,67 +90,67 @@ def chance(position, positions, log):
 
     if draw == 1:
 
-        log.write(f"Chance card. Go to Jail ")
+        log.write(f"Chance card. Go to Jail. ")
         position = 10
 
 
     elif draw == 2:
 
-        log.write(f"Chance card. Go to Go ")
+        log.write(f"Chance card. Go to Go. ")
         position = 0
 
     elif draw == 3:
 
-        log.write(f"Chance card. Go to 24 ")
+        log.write(f"Chance card. Go to 24. ")
         position = 24
 
     elif draw == 4:
 
-        log.write(f"Chance card. Go to 11 ")
+        log.write(f"Chance card. Go to 11. ")
         position = 11
 
     elif draw == 5:
 
-        log.write(f"Chance card. Go to 5 ")
+        log.write(f"Chance card. Go to 5. ")
         position = 5
 
     elif draw == 6:
 
-        log.write(f"Chance card. Go to 39 ")
+        log.write(f"Chance card. Go to 39. ")
         position = 39
 
     elif draw == 7:
 
         if position < 12 or position >= 28:
 
-            log.write(f"Chance card. Go to 12 ")
+            log.write(f"Chance card. Go to 12. ")
             position = 12
 
         else:
 
-            log.write(f"Chance card. Go to 28 ")
+            log.write(f"Chance card. Go to 28. ")
             position = 28
 
     elif draw == 8:
 
         if position >= 35 or position < 5:
 
-            log.write(f"Chance card. Go to 5 ")
+            log.write(f"Chance card. Go to 5. ")
             position = 5
 
         elif position >=5 and position <15:
 
-            log.write(f"Chance card. Go to 15 ")
+            log.write(f"Chance card. Go to 15. ")
             position = 15
 
         elif position >= 15 and position < 25:
 
-            log.write(f"Chance card. Go to 25 ")
+            log.write(f"Chance card. Go to 25. ")
             position = 25
 
         elif position >= 25 and position < 35:
 
-            log.write(f"Chance card. Go to 25 ")
+            log.write(f"Chance card. Go to 35. ")
             position = 35
 
     elif draw == 9:
@@ -158,11 +158,11 @@ def chance(position, positions, log):
         if position != 36:
 
             position -=3
-            log.write(f"Chance card. Go to {position - 3} ")
+            log.write(f"Chance card. Go to {position - 3}. ")
 
         else:
 
-            log.write(f"Chance card. Go to {position - 3} ")
+            log.write(f"Chance card. Go to {position - 3}. ")
             position -= 3
             positions[position] +=1
             position, positions = community_chest(position, positions, log)
@@ -172,7 +172,7 @@ def chance(position, positions, log):
 #Continue here!!!! go back 3 spaces!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     else:
 
-        log.write(f"Chance card.No Movement ")
+        log.write(f"Chance card.No Movement. ")
 
     positions[position] +=1
     return position, positions
@@ -183,11 +183,10 @@ rounds = 0
 position = 0
 max_position = 39
 dice = 0
-datum = datetime.datetime.now()
-logname = f"log_{datum.strftime('%H-%M-%S')}.txt"
+date = datetime.datetime.now()
+logname = f"log_{date.strftime('%H-%M-%S')}.txt"
 log = open(logname, "x")
-
-positions = felder()
+positions = board()
 
 
 #######################################################################
@@ -196,11 +195,11 @@ positions = felder()
 
 for rounds in range(0, max_rounds):
 
-    log.write(f"position vor wurf: {position} ")
+    log.write(f"Position before dice throw: {position} ")
     dice = random.randint(2, 12)
-    log.write(f"Wurf: {dice} ")
+    log.write(f"Dice: {dice} ")
     position = overflow(max_position, position, dice)
-    log.write(f"Position nach wurf ohne Jail: {position} ")
+    log.write(f"Position after dice throw, before chance/community/Go to jail: {position} ")
 
     if position == 2 or position == 17 or position == 3:
         position, positions = community_chest(position, positions, log)
@@ -212,7 +211,7 @@ for rounds in range(0, max_rounds):
     else:
         position, positions = jail(position, positions, log)
 
-    log.write(f"Position: {position}\n")
+    log.write(f"Position at the end of the turn: {position}\n")
 
 log.write(f"\n\n")
 
@@ -220,5 +219,5 @@ for i in positions:
 
     log.write(f"{i}\n")
 
-log.write(f"\n\n Welches Feld wie oft: {positions}\n")
+log.write(f"\n\n Which position how often: {positions}\n")
 log.close()
